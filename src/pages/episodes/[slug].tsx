5ff353628010043be81154501408312df4bd6e76
Lucas Cardoso <83316119+devlucascardoso@users.noreply.github.com>
2021-05-03 23:56:56 -0300

    Update [slug].tsx

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,6 +1,6 @@
 import { format, parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
-import { GetStaticPaths, GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next"; 
 import Image from "next/image";
 import Link from "next/link";
 
@@ -61,9 +61,25 @@ export default function Episode({ episode }: EpisodeProps) {
   );
 }
 
-export const getStaticPaths: GetStaticPaths = async () => {
+  export const getStaticPaths: GetStaticPaths = async () => {
+    const { data } = await api.get("episodes", {
+      params: {
+        _limit: 2,
+        _sort: "published_at",
+        _order: "desc",
+      },
+    });
+  
+    const paths = data.map((episode) => {
+      return {
+        params: {
+          slug: episode.id,
+        },
+      };
+    });
+
   return {
-    paths: [],
+    paths,
     fallback: "blocking",
   };
 };
